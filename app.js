0baea3f87414e3af676494a92399e9d4c01a1b23
James Coutry <jcoutry@perfectsensedigital.com>
2012-06-08 04:03:48 -0400

    /:user determinds what use to query

--- app.js
+++ app.js
@@ -1,126 +1,118 @@
 var express = require("express");
 var yt = require('youtube-data');
 var fs = require("fs");
-
-
-var YouTube = function(id, title, url, pub, description){
-    // type checking
-	if(typeof id === "undefined") return console.error("An error occured.. Missing id");
-	if(typeof title === "undefined") return console.error("An error occured.. Missing title");
-	if(typeof url === "undefined") return console.error("An error occured.. Missing url");
-
-	// Constructor
-	this.id = id;
-	this.title = title;
-	this.permalink = url;
-	this.publishDate = pub;
-	this.description = description || "";
-};
-
 var app = express.createServer();
 
-app.get('/', function(req, res){
-
-	var videos = [];
-	var startingNumber = 1;
-	var isComplete = false;
-
-	var template_row = [
-		'<tr>\n',
-			'<td><table>{{object}}<tr><td></td><td></td></tr><tr><td></td><td></td></tr></table></td>\n',
-		'</tr>\n\n'
-	].join("");
-
-	var template_data = [
-		'<tr>\n',
-			'<td>{{key}}</td>\n',
-			'<td>{{value}}</td>\n',
-		'</tr>\n'
-	].join("");
-
-	var result = "";
-
-	var q = function() {
-		
-		yt.query()
-			.videos('cocacola')
-			.results(25)
-			.startAt( startingNumber )
-			.all()
-			.run(function(err, data){
-				
-				if (err) return console.error(err);
-
-				for (var i = 0; i < data.feed.entry.length; i++) {
-
-					var e = data.feed.entry[i];
-
-					var _id = e["media$group"]["yt$videoid"]["$t"];
-					var _title = e["title"]["$t"];
-					var _url = e["link"][0]["href"];
-					var _pub = e["published"]["$t"];
-					var _desc = e["media$group"]["media$description"]["$t"];
+var YouTube = function(id, title, url, pub, description){
+  if(typeof id === "undefined") return console.error("An error occured.. Missing id");
+  if(typeof title === "undefined") return console.error("An error occured.. Missing title");
+  if(typeof url === "undefined") return console.error("An error occured.. Missing url");
+  this.id = id;
+  this.title = title;
+  this.permalink = url;
+  this.publishDate = pub;
+  this.description = description || "";
+};
 
 
-					var Video = new YouTube(_id, _title, _url, _pub, _desc);
 
-					videos.push(Video);
+app.get('/:user', function(req, res){
 
-				};
+  console.log("\n\nData for: " + req.params.user);
 
-				startingNumber = startingNumber + 25;
+  if (typeof req.params.user === "undefined") {
+    return res.end("count or user not found");
+  };
 
-				if (startingNumber > 100) {
-					isComplete = true;
-				};
+  var videos = [];
+  var startingNumber = 1;
+  var isComplete = false;
 
-				console.log(startingNumber, isComplete);
+  var template_row = [
+    '<tr>\n',
+      '<td><table>{{object}}<tr><td></td><td></td></tr><tr><td></td><td></td></tr></table></td>\n',
+    '</tr>\n\n'
+  ].join("");
 
-				if (isComplete) {
+  var template_data = [
+    '<tr>\n',
+      '<td>{{key}}</td>\n',
+      '<td>{{value}}</td>\n',
+    '</tr>\n'
+  ].join("");
 
-					for (var i = 0; i < videos.length; i++) {
-						var v = videos[i];
-						var _temp = "";
+  var result = "";
 
-						for(var o in v){
-							_temp += template_data
-								.replace("{{key}}", o)
-								.replace("{{value}}", v[o]);
-						};
+  var the_query = function() {
 
-						result += template_row.replace("{{object}}", _temp);
+    yt.query().videos(req.params.user).results(25).startAt( startingNumber ).all().run(function(err, data){
 
-					};
+      if (err) return console.error(err);
 
-					// return res.json(videos);
-					fs.writeFile("videos.json", JSON.stringify(videos), function(err) {
-						if(err) {
-							console.log(err);
-						} else {
-							console.log("The json file was saved!");
-						};
-					}); 
+        for (var i = 0; i < data.feed.entry.length; i++) {
+          var e = data.feed.entry[i];
+          var _id = e["media$group"]["yt$videoid"]["$t"];
+          var _title = e["title"]["$t"];
+          var _url = e["link"][0]["href"];
+          var _pub = e["published"]["$t"];
+          var _desc = e["media$group"]["media$description"]["$t"];
 
-					// return res.json(videos);
-					fs.writeFile("videos.xls", "<html>\n<head></head>\n<body>\n<table>\n<tbody>\b" + result + "</tbody>\n</table>\n</body>\n</html>", function(err) {
-						if(err) {
-							console.log(err);
-						} else {
-							console.log("The html file was saved!");
-						};
-					}); 
+          var Video = new YouTube(_id, _title, _url, _pub, _desc)
+          videos.push(Video);
+        };
 
-					return res.end();
-				};
+        startingNumber = startingNumber + 25;
 
-				q();
+        if (startingNumber > 976) {
+          isComplete = true;
+        };
 
-			});
+        console.log(startingNumber, isComplete);
 
-	};
+        if (isComplete) {
+          for (var i = 0; i < videos.length; i++) {
+            var v = videos[i];
+            var _temp = "";
 
-	q();
-	
+            for(var o in v){
+              _temp += template_data
+                .replace("{{key}}", o)
+                .replace("{{value}}", v[o]);
+            };
+
+            result += template_row.replace("{{object}}", _temp);
+        };
+
+        // creates json stringified file
+        fs.writeFile("videos.json", JSON.stringify(videos), function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("\n\n\nThe json file was saved!");
+          };
+        }); 
+
+        // creates excel file
+        fs.writeFile("videos.xls", "<html>\n<head></head>\n<body>\n<table>\n<tbody>\b" + result + "</tbody>\n</table>\n</body>\n</html>", function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("The html file was saved!");
+          };
+        }); 
+
+        console.log(videos.length + " entries were printed!");
+
+        return res.end("<h2>FINISHED! There were " + videos.length + " entries written!</h2>");
+      };
+
+      // recursively run "the_query"
+      the_query();
+
+    });
+  };
+
+  the_query();
 
 });
 
